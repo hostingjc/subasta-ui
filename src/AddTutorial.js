84631b065cc0668004df27119c58608a727d946f
hostingjc <hosting@juandiaz.co.ve>
2021-03-07 11:08:25 -0400

    add routes

---
+++ src/AddTutorial.js
@@ -0,0 +1,8 @@
+const AddTutorial = () => {
+    return (
+            <h1>HOla Add tutorial</h1>
+      );
+}
+
+
+export default AddTutorial;
---
+++ src/TutorialList.js
@@ -0,0 +1,8 @@
+const TutorialList = () => {
+    return (
+            <h1>LIst tutorials</h1>
+      );
+}
+
+
+export default TutorialList;
