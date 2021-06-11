eb3bb16aed1b9f382eea98a238f7d13e67d98f9a
hostingjc <hosting@juandiaz.co.ve>
2021-06-11 13:07:36 -0400

    cambio de dominio

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-
-axios.defaults.baseURL = 'http://subasta.com/api/';
+axios.defaults.baseURL = 'https://subastavozalta.herokuapp.com/api/';
+//axios.defaults.baseURL = 'http://subasta.com/api/';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.render(
