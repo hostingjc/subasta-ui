3860679aca103714a8948edc4b65a16211515b05
hostingjc <hosting@juandiaz.co.ve>
2021-06-14 15:18:08 -0400

    add fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AuthProvider from "./auth/AuthProvider";
 import useScript from "./hooks/useScript";
 
 import AppRouter from "./routers/AppRouter";
-import jQuery from 'jquery';
+
 
 
 import './assets/wp-content/plugins/LayerSlider/static/layerslider/css/layerslider2cfc.css';
@@ -18,44 +18,23 @@ import './assets/wp-content/themes/betheme/assets/animations/animations.mine17d.
 import './assets/wp-content/themes/betheme/assets/ui/jquery.ui.alle17d.css?ver=20.9.5.1';
 import './assets/wp-content/themes/betheme/assets/jplayer/css/jplayer.blue.mondaye17d.css?ver=20.9.5.1'; 
 import './assets/wp-content/themes/betheme/css/responsivee17d.css?ver=20.9.5.1'; 
+import './assets/fonts.css';
 import './assets/style1.css';
 import { useEffect } from "react";
-import ScriptTag from 'react-script-tag';
+
 
 function App() {
 
-
-  //window.jQuery = jQuery;
-
-
-
-
   useEffect(() => {
     document.body.className = 'page-template page-template-template-portfolio page-template-template-portfolio-php page page-id-39  color-custom style-default button-flat layout-full-width if-zoom if-border-hide hide-love no-shadows header-classic header-fw header-boxed minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menu-link-color menuo-right menuo-no-borders mobile-tb-hide mobile-mini-mr-ll tablet-sticky mobile-sticky be-reg-20951 wpb-js-composer js-comp-ver-6.0.2 vc_responsive';
-
-  
-
-
     return () => { document.body.className = ''; }
   });
-
-
 
   return (
     
       <AuthProvider>
-
-
-
-        <AppRouter />
-      
-        
-     
+        <AppRouter />        
       </AuthProvider>
-      
-
-
-
  
   );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,20 +42,37 @@ export default function Navbar() {
 
 
 
-
-
-
-
-
-
       <div className="menu_wrapper">
-        <nav id="menu"><ul id="menu-menu" className="menu menu-main"><li id="menu-item-102" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home"><a href="../index.html"><span>Inicio</span></a></li>
-<li id="menu-item-100" className="menu-item menu-item-type-post_type menu-item-object-page"><a href="../introduccion/index.html"><span>Introducción</span></a></li>
-<li id="menu-item-101" className="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-39 current_page_item"><a href="index.html"><span>Catálogo</span></a></li>
-<li id="menu-item-223" className="menu-item menu-item-type-post_type menu-item-object-page"><a href="../artistas/index.html"><span>Artistas</span></a></li>
-<li id="menu-item-227" className="menu-item menu-item-type-post_type menu-item-object-page"><a href="../condiciones/index.html"><span>Condiciones</span></a></li>
-<li id="menu-item-96" className="menu-item menu-item-type-post_type menu-item-object-page"><a href="../precios/index.html"><span>Precios/Ofertar</span></a></li>
-</ul></nav>
+        <nav id="menu">
+          <ul id="menu-menu" className="menu menu-main">
+          <li id="menu-item-102" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home">
+        <NavLink exact to="/" activeClassName="active">
+           <span> Home</span>
+          </NavLink></li>
+<li id="menu-item-100" className="menu-item menu-item-type-post_type menu-item-object-page">
+<NavLink exact to="/about" activeClassName="active">
+<span>   About </span>
+          </NavLink></li>
+<li id="menu-item-101" className="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-39 current_page_item">
+<NavLink exact to="/contact" activeClassName="active">
+<span>  Contact  </span>
+          </NavLink></li>
+<li id="menu-item-223" className="menu-item menu-item-type-post_type menu-item-object-page">
+<NavLink to="/categories" activeClassName="active">
+<span>         Categories </span>
+          </NavLink></li>
+<li id="menu-item-227" className="menu-item menu-item-type-post_type menu-item-object-page">
+<NavLink to="/categories" activeClassName="active">
+<span>   Categories</span>
+          </NavLink></li>
+<li id="menu-item-96" className="menu-item menu-item-type-post_type menu-item-object-page">
+<NavLink to="/categories" activeClassName="active">
+<span>    Categories </span>
+          </NavLink>
+  </li>
+</ul>
+
+</nav>
 
 <a className="responsive-menu-toggle " href="#"><i className="icon-menu-fine"></i></a>					
       </div>	
